fb91d768b25e1bb68ba80f7e36827397d9ae2ee4
Esteban Mortier <est.mortier@gmail.com>
2022-10-02 22:08:19 +0200

    Minor update for posts

--- requests/facebook.request.js
+++ requests/facebook.request.js
@@ -10,7 +10,7 @@ const pageId = process.env.FACEBOOK_PAGE_ID;
 
 request.post(
     `https://graph.facebook.com/v15.0/${pageId}/feed?access_token=${token}`,
-    { form: { message: '65465154' } },
+    { form: { message: '65465148554' } },
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body)
