15be670e016840abb7321c99ab693c6c0360004d
Esteban Mortier <64356124+Esteban-Mo@users.noreply.github.com>
2022-11-17 14:29:19 +0100

    Add archive system
    Co-authored-by: Alexandre MATHIEU <Nauwk07@users.noreply.github.com>

--- src/scripts/auto-post.js
+++ src/scripts/auto-post.js
@@ -7,6 +7,7 @@
 //
 
 const post = require(`${appRoot}/src/models/mongoDB/posts.model`);
+const archiveModel = require(`${appRoot}/src/models/mongoDB/archives.model`);
 const { postFacebook, postFacebookWithPicture } = require(`${appRoot}/src/scripts/facebook-api`);
 const { postTweet, postTweetWithPicture } = require(`${appRoot}/src/scripts/twitter-api`);
 require('dotenv').config();
@@ -38,15 +39,29 @@ setInterval(() => {
 				// Post on social media
 				// ----------------------------------------
 
-				if (post.facebook) {
-					post.media == null || post.media == '' ? postFacebook(post.message) : postFacebookWithPicture(post.message, post.media); // Post on Facebook
-					console.log('Posté sur Facebook');
-				}
+				const archive = new archiveModel.model({
+					message: post.message,
+				});
 
-				if (post.twitter) {
-					post.media == null || post.media == '' ? postTweet(post.message) : postTweetWithPicture(post.message, post.media); // Post on Twitter
-					console.log('Posté sur Twitter');
-				}
+				//
+				// 🚨 SYSTEME A REVOIR
+				//
+
+				archive.save((err, data) => {
+					if (err) return console.log('error', err);
+
+					if (post.facebook) {
+						post.media == null || post.media == '' ? postFacebook(post.message, data._id) : postFacebookWithPicture(post.message, post.media); // Post on Facebook
+					}
+
+					if (post.twitter) {
+						post.media == null || post.media == '' ? postTweet(post.message, data._id) : postTweetWithPicture(post.message, post.media); // Post on Twitter
+					}
+				});
+
+				//
+				// 🚨 SYSTEME A REVOIR
+				//
 
 				// ----------------------------------------
 				// Post on social media
