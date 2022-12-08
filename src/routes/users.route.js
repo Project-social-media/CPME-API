89e0564a31407df8e632c04c5d0de5761c8a91dc
Esteban Mortier <64356124+Esteban-Mo@users.noreply.github.com>
2022-12-08 19:51:09 +0100

    Update

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const { getAll, getById, getByIdInJwtToken, create, update, deleteD } = require(`${appRoot}/src/controllers/users.controller`);
 
 router
-    .get('/', getAll)
-    .get('/:id', getById)
+    .get('/all', getAll)
+    .get('/id/:id', getById)
     .get('/me', getByIdInJwtToken)
-    .post('/', create)
-    .put('/:id', update)
-    .delete('/:id', deleteD);
+    .post('/create', create)
+    .put('/update/:id', update)
+    .delete('/delete/:id', deleteD);
 
 module.exports = router;
